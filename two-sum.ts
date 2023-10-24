f582f5a48f48c1df57fa4dcbc2bd6fbd5411052f
Justus Brown <justus.c.brown@gmail.com>
2023-10-24 12:01:22 -0700

    two-sum

--- two-sum.ts
+++ two-sum.ts
@@ -1,1 +1,29 @@
-console.log("hello")
+function twoSum(nums: number[], target: number): number[] {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+
+        }
+
+    }
+    return [-1, -1]
+
+};
+console.log(twoSum([2, 7, 11, 15], 9))
+
+
+// FASTEST Solution from Leet:
+
+// O(n) - One-pass Hash Table
+// var twoSum = function(nums, target) {
+//     let map = new Map;
+//     for (var i = 0; i < nums.length; i++) {
+//         let complement = target - nums[i];
+//         if (map.has(complement)) {
+//             return [map.get(complement), i]
+//         }
+//         map.set(nums[i], i);
+//     }
+// }
